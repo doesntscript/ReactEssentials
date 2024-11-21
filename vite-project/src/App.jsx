4a24602e3dc0f53715113c1cdae4f1bf9f15003b
Guilherme <doesntscript@gmail.com>
2024-11-21 10:59:00 -0300

    (AuthPags): Atualizando Componentes Reutilizaveis com props

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,6 +5,10 @@
 
 // Como importar imagens do React
 import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
+import configImg from './assets/config.png';
+import jsxImg from './assets/jsx-ui.png';
+import stateImg from './assets/state-mgmt.png';
 
 import { useEffect, useState } from "react";
 
@@ -42,12 +46,34 @@ function Header() {
   )
 }
 
+function CoreConcept(props) {
+  return (
+    <>
+    <li>
+      <img src={props.img} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+    </>
+  )
+}
+
 
 function App() {
     return (
       <div>
        <Header />
         <main>
+          <section id='core-concepts'>
+            <h2>Core Concepts</h2>
+            <ul>
+              <CoreConcept img={componentsImg} title="Components" description="The core UI building block."  />
+              <CoreConcept img={configImg} title="Props" description="" />
+              <CoreConcept img={jsxImg} title="" description="" />
+              <CoreConcept img={stateImg} title="" description="" />
+            </ul>
+
+          </section>
           <h2>Time to get started!</h2>
         </main>
       </div>
